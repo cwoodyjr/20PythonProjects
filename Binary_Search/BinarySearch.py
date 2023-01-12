bf074e12ff9752886f699ea737408a16e1fcb9bc
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2023-01-12 10:42:07 +0000

    binary search first attempt

--- Binary_Search/BinarySearch.py
+++ Binary_Search/BinarySearch.py
@@ -1,3 +1,24 @@
-stringToSearch = 'search this string'
+def binarySearch(list, element):
+    start = 0
+    middle = 0
+    end = len(list)
+    steps = 0
 
-print(stringToSearch)
+    while (start <= end):
+        print('Step', steps, ":", str(list[start:end+1]))
+
+        steps = steps+1
+        middle = (start + end) // 2
+
+        if element == list[middle]:
+            return middle
+        if element < list[middle]:
+            end = middle-1
+        else:
+            start = middle+1
+    return -1
+
+
+numList = [1, 2, 3, 4, 5]
+target = 4
+binarySearch(numList, target)
