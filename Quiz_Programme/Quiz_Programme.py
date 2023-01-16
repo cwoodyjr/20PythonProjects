2b3367f7816a6b6abeb4e04f90b5027f9c30866d
Craig Woodcock <92760478+cwoodyjr@users.noreply.github.com>
2023-01-16 23:46:30 +0000

    created quiz with dict

---
+++ Quiz_Programme/Quiz_Programme.py
@@ -0,0 +1,45 @@
+quiz = {
+    "question 1":{
+        "question" : "What is the capital of England?",
+        "answer" : "London"
+    },
+    "question 2":{
+        "question" : "What is the Capital of France?",
+        "answer" : "Paris"
+    },
+     "question 3":{
+        "question" : "What is the only day of the week beginning with 'w'? ",
+        "answer" : "Wednesday"
+    },
+     "question 4":{
+        "question" : "What is the 4th letter of the Alphabet?",
+        "answer" : "D"
+    },
+     "question 5":{
+        "question" : "What is the sun made of?",
+        "answer" : "Gas"
+    }
+}
+
+score = 0
+
+for key, value in quiz.items():
+    print(value["question"])
+    answer = input("Answer? :")
+    print("")
+
+    if answer.lower() == value["answer"].lower():
+        print("Well Done!")
+        score = score +1
+        print("your score is now "+str(score))
+        print("")
+    else:
+        print("Thats incorrect")
+        print("your final score is "+str(score)+"/10")
+        quit(0)
+
+print("congratulations You scored max points!")
+
+
+
+
