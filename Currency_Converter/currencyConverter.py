355dc451893d4dc5d981fe8e39ebea04ae5c61af
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2023-01-18 23:42:30 +0000

    currency convert + added gbpd to usdv + added options function

---
+++ Currency_Converter/currencyConverter.py
@@ -0,0 +1,48 @@
+def usd_to_gbp():
+    print("This will convert USD to GBP")
+    print()
+
+    dollars = eval(input("enter the amount of USD to convert: "))
+
+    pounds = convert_to_gbp(dollars)
+
+    print(dollars, " to GBP is: £", pounds)
+
+
+def convert_to_gbp(dollars): return dollars*0.82
+
+
+def gbp_to_usd():
+    print("this will convert GBP to USD")
+    print()
+
+    pounds = eval(input("enter the amount of GBP to convert: "))
+
+    dollars = convert_to_usd(pounds)
+
+    print(pounds, " to USD is: $", dollars)
+
+
+def convert_to_usd(dollars): return dollars*1.23
+
+
+def options():
+    option = input("what whould you like to do? :")
+
+    print("1. Convert USD To GBP")
+    print("2. convert GBP to USD")
+    print("X. Exit")
+
+    if option == "1":
+        usd_to_gbp()
+    elif option == "2":
+        gbp_to_usd()
+    elif option.lower() == "x".lower():
+        print("Exiting Programme")
+        quit(0)
+    else:
+        print("invalid option")
+        options()
+
+
+options()
