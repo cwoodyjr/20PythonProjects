55c40f77f1f92c37f1c288e2d54837b29989cbba
Craig Woodcock <craig.woodcock89@hotmail.co.uk>
2023-01-18 17:49:48 +0000

    moved dice_drawng dict to own file> added lucky message for doubles> added function to roll doubles only

--- Dice_Roller/Dice_Roller.py
+++ Dice_Roller/Dice_Roller.py
@@ -1,60 +1,42 @@
 import random
+from DiceDict import dice_drawing
+
 
 def roll_dice():
-
-    dice_drawing = {
-        1: (
-            "-----",
-            "|   |",
-            "| o |",
-            "|   |",
-            "-----",
-        ),
-        2: (
-            "-----",
-            "|o  |",
-            "|   |",
-            "|  o|",
-            "-----",
-        ),
-        3: (
-            "-----",
-            "|o  |",
-            "| o |",
-            "|  o|",
-            "-----",
-        ),
-        4: (
-            "-----",
-            "|o o|",
-            "|   |",
-            "|o o|",
-            "-----",
-        ),
-        5: (
-            "-----",
-            "|o o|",
-            "| o |",
-            "|o o|",
-            "-----",
-        ),
-        6: (
-            "-----",
-            "|o o|",
-            "|o o|",
-            "|o o|",
-            "-----",
-        ),
-
-    }
     roll = input("Roll the dice? (Yes/No) : ")
-    while roll.lower() == "Yes". lower():
+    while roll.lower() == "Yes". lower() or roll.lower() == "Y". lower():
         dice1 = random.randint(1, 6)
         dice2 = random.randint(1, 6)
-
         print("dice rolled: {} and {}". format(dice1, dice2))
         print("\n".join(dice_drawing[dice1]))
         print("\n".join(dice_drawing[dice2]))
-        
+        if dice2 == dice1:
+            print("YOU LUCKY PERSON!!")
+            print("")
         roll = input("Roll again? (Yes/no): ")
+    print("exiting...")
+    quit()
+    
+def roll_doubles():
+    roll = input("Roll the dice? (Yes/No) : ")
+    while roll.lower() == "Yes". lower() or roll.lower() == "Y". lower():
+        dice1 = random.randint(1, 6)  
+        dice2 = dice1
+        #in ths method we can create 2 instances of dice and make thge 2nd egual to the 1st.
+        #or we can use a while loop to generate a random int on the 2nd dice until it equals the 1st
+        # e.g:
+        # while dice2 != dice1:
+        #     dice2 = random.randint(1,6)
+            
+        print("dice rolled: {} and {}". format(dice1, dice2))
+        print("\n".join(dice_drawing[dice1]))
+        print("\n".join(dice_drawing[dice2]))
+        roll = input("Roll again? (Yes/no): ")
+    print("exiting...")
+    quit()
+
+
+user_answer = input("should we roll doubles only? ")
+while user_answer.lower() == "Yes". lower() or user_answer.lower() == "Y". lower():
+    roll_doubles()
 roll_dice()
