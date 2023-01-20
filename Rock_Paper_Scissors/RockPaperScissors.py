c9db92db6e2756314573d85d9a4df71e296244f7
Craig Woodcock <92760478+cwoodyjr@users.noreply.github.com>
2023-01-20 22:53:03 +0000

    rock paper scissors attempt

--- Rock_Paper_Scissors/RockPaperScissors.py
+++ Rock_Paper_Scissors/RockPaperScissors.py
@@ -3,14 +3,53 @@ import random
 exit = False
 
 while exit == False:
-    options = ["Rock", "Paper", "Scissors"]
-    user_input = input("Choose Rock, Paper, Scissors or Exit: ")
-    print("You Chose: ", user_input)
-    computer_input = random.choice(options)
-    print("Computer Chose: ", computer_input)
+    for x in range(6):
+        options = ["Rock", "Paper", "Scissors"]
+        print("Attempt number: ",x)
+        user_input = input("Choose Rock, Paper, Scissors or Exit: ")
+        print("You Chose: ", user_input)
+        computer_input = random.choice(options)
+        print("Computer Chose: ", computer_input)
 
-    if user_input.lower() == "Exit".lower():
-        print("Exiting Programme")
-        exit = True
-    if computer_input.lower() == user_input.lower():
-        print("Thats a Tie!!")
+        user_score = 0
+        computer_score = 0
+
+        if user_input.lower() == "Exit".lower():
+            print("Exiting Programme")
+            exit = True
+        elif computer_input.lower() == user_input.lower():
+            print("Thats a Tie!!")
+        elif computer_input.lower() == "Rock".lower():
+            if user_input.lower() == "Paper":
+                print("YOU WIN!")
+                user_score += 1
+            else:
+                print("Comouter Wins!")
+                computer_score += 1
+        elif computer_input.lower() == "Paper".lower():
+            if user_input.lower() == "Scissors".lower():
+                print("YOU WIN!!")
+                user_score += 1
+            else:
+                print("computer Wins!")
+                computer_score += 1
+        elif computer_input.lower() == "Scissors".lower():
+            if user_input.lower() == "Rock".lower():
+                print("YOU WIN!!")
+                user_score += 1
+            else:
+                print("computer Wins!")
+                computer_score += 1
+        if x == 5:
+            exit = True
+
+print("Thats the end of the game")
+print("You Scored ",user_score)
+print("Computer Scored", computer_score)
+
+if user_score > computer_score:
+    print("YOU BEAT THE COMPUTER")
+elif computer_score > user_score:
+    print("YOU LOST THE GAME")
+else:
+    print("THE GAME ENDED IN A TIE")
